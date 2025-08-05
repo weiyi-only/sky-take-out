b7960360a7035538ad25c94ffccf9f98ec6ae963
weiyi-only <19877054642@163.com>
2025-08-05 22:21:48 +0800

    公共字段自动填充代码实现

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,9 +1,11 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.dto.PasswordEditDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -29,6 +31,7 @@ public interface EmployeeMapper {
     @Insert("insert into employee(name,username,password,phone,sex,id_number,status,create_time,update_time,create_user,update_user)" +
             "values (#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status}," +
             "    #{createTime},#{updateTime},#{createUser},#{updateUser})")
+    @AutoFill(value= OperationType.INSERT)
     void insert(Employee employee);
 
     /**
@@ -42,6 +45,7 @@ public interface EmployeeMapper {
      * 启用禁用员工账号
      * @param employee
      */
+    @AutoFill(value= OperationType.UPDATE)
     void update(Employee employee);
 
     /**
