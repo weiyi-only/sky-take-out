442800255bbb6361e1e4ef16e575b0912779b290
weiyi-only <19877054642@163.com>
2025-08-12 22:05:26 +0800

    客户催单功能代码

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -482,4 +482,18 @@ public class OrderServiceImpl implements OrderService {
 
         orderMapper.update(orders);
     }
+    //客户催单
+    public void reminder(Long id){
+        // 根据id查询订单
+        Orders ordersDB = orderMapper.getById(id);
+        // 校验订单是否存在
+        if (ordersDB == null) {
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+        Map map = new HashMap();
+        map.put("type",2);
+        map.put("orderId",id);
+        map.put("content","订单号："+ordersDB.getNumber());
+        webSocketServer.sendToAllClient(JSON.toJSONString(map));
+    }
 }
